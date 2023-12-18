67882cd6633b05d5b36b5e901a32e5e9e7916721
Lucas Daniel Costa Da Silva <ldcs@cin.ufpe.br>
2023-12-18 15:17:23 -0300

    <feat> update conference publication with new fields

--- src/professor/dto/conference.dto.ts
+++ src/professor/dto/conference.dto.ts
@@ -21,6 +21,18 @@ export class ConferenceDto {
   @ApiProperty({ name: 'proceedings', type: String })
   proceedings!: string;
 
+  @ApiProperty({ name: 'bigArea', type: String, nullable: true })
+  bigArea?: string;
+
+  @ApiProperty({ name: 'journaarealTitle', type: String, nullable: true })
+  area?: string;
+
+  @ApiProperty({ name: 'subArea', type: String, nullable: true })
+  subArea?: string;
+
+  @ApiProperty({ name: 'speciality', type: String, nullable: true })
+  speciality?: string;
+
   @ApiProperty({ name: 'authors', type: String })
   authors!: string;
 }
