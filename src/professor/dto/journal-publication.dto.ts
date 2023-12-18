82e7016d4c5ad93101d6d2f0b139ab0fe32b21e5
Lucas Daniel Costa Da Silva <ldcs@cin.ufpe.br>
2023-12-18 15:17:38 -0300

    <feat> update journal publication with new fields

--- src/professor/dto/journal-publication.dto.ts
+++ src/professor/dto/journal-publication.dto.ts
@@ -21,6 +21,18 @@ export class JournalPublicationDto {
   @ApiProperty({ name: 'journalTitle', type: String, nullable: true })
   journalTitle?: string;
 
+  @ApiProperty({ name: 'bigArea', type: String, nullable: true })
+  bigArea?: string;
+
+  @ApiProperty({ name: 'journaarealTitle', type: String, nullable: true })
+  area?: string;
+
+  @ApiProperty({ name: 'subArea', type: String, nullable: true })
+  subArea?: string;
+
+  @ApiProperty({ name: 'speciality', type: String, nullable: true })
+  speciality?: string;
+
   @ApiProperty({ name: 'authors', type: String, nullable: true })
   authors?: string;
 }
