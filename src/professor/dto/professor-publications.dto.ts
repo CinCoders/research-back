0b71ab362df234bd1fab0c28989600fe3fb19238
eliseucbrito <ecb2@cin.ufpe.br>
2025-03-20 12:42:01 -0300

    refactor: add "isEvent" property to ProfessorPublicationsDto

--- src/professor/dto/professor-publications.dto.ts
+++ src/professor/dto/professor-publications.dto.ts
@@ -2,30 +2,33 @@ import { ApiProperty, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('ProfessorPublicationsDto')
 export class ProfessorPublicationsDto {
-  @ApiProperty({ name: 'title', type: String })
-  title!: string;
+	@ApiProperty({ name: 'title', type: String })
+	title!: string;
 
-  @ApiProperty({ name: 'eventJournal', type: String })
-  eventJournal!: string | null;
+	@ApiProperty({ name: 'eventJournal', type: String })
+	eventJournal!: string | null;
 
-  @ApiProperty({ name: 'acronymIssn', type: String })
-  acronymIssn!: string | null;
+	@ApiProperty({ name: 'eventJournal', type: Boolean })
+	isEvent!: boolean;
 
-  @ApiProperty({ name: 'type', type: String, enum: ['journal', 'conference'] })
-  type!: string;
+	@ApiProperty({ name: 'acronymIssn', type: String })
+	acronymIssn!: string | null;
 
-  @ApiProperty({ name: 'year', type: Number })
-  year!: number;
+	@ApiProperty({ name: 'type', type: String, enum: ['journal', 'conference'] })
+	type!: string;
 
-  @ApiProperty({ name: 'qualis', type: String })
-  qualis?: string;
+	@ApiProperty({ name: 'year', type: Number })
+	year!: number;
 
-  @ApiProperty({ name: 'isTop', type: Boolean })
-  isTop?: boolean;
+	@ApiProperty({ name: 'qualis', type: String })
+	qualis?: string;
 
-  @ApiProperty({ name: 'authors', type: String })
-  authors!: string;
+	@ApiProperty({ name: 'isTop', type: Boolean })
+	isTop?: boolean;
 
-  @ApiProperty({ name: 'doi', type: String, nullable: true })
-  doi?: string;
+	@ApiProperty({ name: 'authors', type: String })
+	authors!: string;
+
+	@ApiProperty({ name: 'doi', type: String, nullable: true })
+	doi?: string;
 }
