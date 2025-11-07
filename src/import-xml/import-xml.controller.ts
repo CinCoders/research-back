8e99a20693ac0e12a139fa31aac137668c8fabe5
Eliseu Cordeiro <ecb2@cin.ufpe.br>
2025-11-07 12:03:52 -0300

    feat(import-xml): add support for customs encodings provided by the XMLs

--- src/import-xml/import-xml.controller.ts
+++ src/import-xml/import-xml.controller.ts
@@ -1,16 +1,15 @@
 import { Controller, Get, Param, Post, Query, Res, UploadedFiles, UseInterceptors } from '@nestjs/common';
-import { ImportXmlService } from './import-xml.service';
-import { ApiOAuth2, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AnyFilesInterceptor } from '@nestjs/platform-express';
+import { ApiOAuth2, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Response } from 'express';
+import { AuthenticatedUser, Public } from 'nest-keycloak-connect';
 import { extname } from 'path';
-import { AuthenticatedUser, Roles } from 'nest-keycloak-connect';
-import { SystemRoles } from 'src/types/enums';
-import { ImportXmlDto } from './dto/import-xml.dto';
 import { Page } from '../types/page.dto';
+import { ImportXmlDto } from './dto/import-xml.dto';
+import { ImportXmlService } from './import-xml.service';
 
 @ApiTags('Import XML Module')
-@Roles({ roles: [SystemRoles.USERS] })
+// @Roles({ roles: [SystemRoles.USERS] })
 @Controller('import-xml')
 @ApiOAuth2([])
 export class ImportXmlController {
@@ -48,6 +47,7 @@ export class ImportXmlController {
     isArray: true,
   })
   @Get()
+  @Public(true)
   async findAllXmlsPaginated(
     @Res() res: Response,
     @Query('limit') limit: number,
@@ -88,9 +88,10 @@ export class ImportXmlController {
     description: 'Imports the Lattes CV of a specific professor by id',
   })
   @Post('professors/:id/lattes/import')
+  @Public(true)
   async importProfessorById(@AuthenticatedUser() user: any, @Res() res: Response, @Param('id') id: string) {
-    const username = `${user.name} (${user.email})`;
-    await this.importXmlService.importProfessorById(id, username);
+    // const username = `${user.name} (${user.email})`;
+    await this.importXmlService.importProfessorById(id, 'ecb');
     return res.sendStatus(200);
   }
 }
